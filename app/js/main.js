2302bb77f8098582ebf23e54a6dc0c256b487a4e
Gavgav2022 <svgavrilenko1994@gmail.com>
2023-02-09 20:01:52 +0200

    index desk

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    
+});
+
+$(document).ready(function(){
+    $('.reviews__list').slick({
+      dots: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      infinite: false,
+    });
+  });
